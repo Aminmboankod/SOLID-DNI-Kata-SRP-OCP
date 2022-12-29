8cfd1908d83acdeadf466592bba83392461f1c42
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2022-12-29 16:36:29 +0100

    feat(checkDni): Add feature check DNI and return correct or incorrect number

--- src/searchLetterDNI.py
+++ src/searchLetterDNI.py
@@ -1,4 +1,14 @@
 
 
 class Dni:
+    
 
+
+    @staticmethod
+    def checkDni(num):
+        if len(num) == 8 and num.isdigit():
+            return "Número DNI correcto"
+        return "Número DNI incorrecto"
+
+
+    
--- test/test_searchLetterDNI.py
+++ test/test_searchLetterDNI.py
@@ -3,10 +3,17 @@ import pytest
 
 @pytest.mark.checkDni
 def test_searchLetterDNI():
-    assert "El número no es correcto" == Dni.checkNumDni("45312")
-    assert "Número DNI correcto" == Dni.checkNumDni("45301872")
+    assert "Número DNI incorrecto" == Dni.checkDni("45312")
+    assert "Número DNI incorrecto" == Dni.checkDni("4531239495834821")
+    assert "Número DNI incorrecto" == Dni.checkDni("45301RE2")
+    assert "Número DNI incorrecto" == Dni.checkDni("ªªªªª>>>")
+    assert "Número DNI incorrecto" == Dni.checkDni("        ")
+    assert "Número DNI incorrecto" == Dni.checkDni("1234Hola")
+    assert "Número DNI correcto" == Dni.checkDni("45301872")
+    assert "Número DNI correcto" == Dni.checkDni("06523123")
 
-@pytest.mark.assignLetter
-def test_searchLetterDNI():
-    assert "Z" == Dni.checkLetter("45301872")
+
+# @pytest.mark.assignLetter
+# def test_searchLetterDNI():
+#     assert "Z" == Dni.checkLetter("45301872")
 
